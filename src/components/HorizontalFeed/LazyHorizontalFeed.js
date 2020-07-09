ef476330fc210c7e51aa1e1d00d2873586ebc968
thiago <tgnemecek@gmail.com>
2020-07-09 19:42:06 -0400

    Fix Link import

--- src/components/HorizontalFeed/LazyHorizontalFeed.js
+++ src/components/HorizontalFeed/LazyHorizontalFeed.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { theme } from "../../theme";
+import { theme } from "components/theme";
 import {
   Box,
   Typography,
