701b026d1ca1965e267e7385aca777c3c8f521ac
thiagonemecek <tgnemecek@gmail.com>
2020-10-27 22:00:08 -0400

    Fix in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: `${__dirname}/static/admin/index.js`,
       },
     },
     {
