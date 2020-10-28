48fd8ecd6facb84517300ba458b9c366ff4ba5cd
thiagonemecek <tgnemecek@gmail.com>
2020-10-27 20:39:46 -0400

    Testing custom video widget

---
+++ src/cms/VideoWidget/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const VideoWidget = ({ value, field, forID, classNameWrapper, onChange }) => {
+  console.log({
+    componentLogging: "VideoWidget",
+    value,
+    field,
+    forID,
+    classNameWrapper,
+    onChange,
+  });
+  return null;
+};
+
+export default VideoWidget;
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,19 @@
 import CMS from "netlify-cms-app";
 import cloudinary from "netlify-cms-media-library-cloudinary";
+import VideoWidget from "./VideoWidget";
 
 CMS.registerMediaLibrary(cloudinary);
+
+const Preview = (value, field, metadata, getAsset, fieldsMetaData) => {
+  console.log({
+    componentLogging: "Preview",
+    value,
+    field,
+    metadata,
+    getAsset: getAsset.toString(),
+    fieldsMetaData,
+  });
+  return null;
+};
+
+CMS.registerWidget("video", VideoWidget, Preview, "string");
