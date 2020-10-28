536c551c38e7c809263c5ad1debcbd271e1c41fa
thiagonemecek <tgnemecek@gmail.com>
2020-10-27 22:18:13 -0400

    Add missing imports

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import CMS from "netlify-cms-app";
 import ReactDOM from "react-dom";
+import cloudinary from "netlify-cms-media-library-cloudinary";
+import VideoWidget from "./VideoWidget";
 import config from "./config";
 
 CMS.registerMediaLibrary(cloudinary);
