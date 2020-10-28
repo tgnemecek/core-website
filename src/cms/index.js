fcab7dbf6361202bde1c02058064b05b68ef2db4
thiagonemecek <tgnemecek@gmail.com>
2020-10-27 22:17:32 -0400

    Change from json to js

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CMS from "netlify-cms-app";
 import ReactDOM from "react-dom";
-import config from "./config.json";
+import config from "./config";
 
 CMS.registerMediaLibrary(cloudinary);
 CMS.registerWidget("video", VideoWidget, Preview, "string");
