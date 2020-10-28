5e84d1885704b511c812e21aadfb5e8bac6efe6a
thiagonemecek <tgnemecek@gmail.com>
2020-10-27 21:29:29 -0400

    Add className

--- src/cms/VideoWidget/index.js
+++ src/cms/VideoWidget/index.js
@@ -19,17 +19,19 @@ const VideoWidget = ({
 
   const isValid = () => {
     const valid = isVideoValid(value);
-    if (!valid)
+    if (!valid) {
       return {
         error: {
           message: "The provided link is invalid. Please double check.",
         },
       };
+    }
     return true;
   };
 
   return (
     <input
+      className={classNameWrapper}
       id={forID}
       onChange={({ target: { value } }) => onChange(value)}
       value={value}
