7ba1cfb3f2bcb132ba5039395b2ac17bf2af0246
thiagonemecek <tgnemecek@gmail.com>
2020-10-27 21:19:19 -0400

    Improve VideoWidget

--- src/cms/VideoWidget/index.js
+++ src/cms/VideoWidget/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { isVideoValid } from "src/util";
 
-const VideoWidget = ({ value, field, forID, classNameWrapper, onChange }) => {
+const VideoWidget = ({
+  value = "",
+  field,
+  forID,
+  classNameWrapper,
+  onChange,
+}) => {
   console.log({
     componentLogging: "VideoWidget",
     value,
@@ -9,7 +16,17 @@ const VideoWidget = ({ value, field, forID, classNameWrapper, onChange }) => {
     classNameWrapper,
     onChange,
   });
-  return null;
+
+  const isValid = () => {
+    return isVideoValid(value);
+  };
+
+  return (
+    <div className={classNameWrapper}>
+      <label htmlFor={forID}>YouTube Link (Optional)</label>
+      <input id={forID} onChange={onChange} value={value} />
+    </div>
+  );
 };
 
 export default VideoWidget;
