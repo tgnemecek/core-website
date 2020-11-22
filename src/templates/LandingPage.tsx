f05064a4039f2ef9293ff9affcd98b41f45fb2a5
thiagonemecek <tgnemecek@gmail.com>
2020-11-22 01:40:59 -0500

    Test

--- src/templates/LandingPage.tsx
+++ src/templates/LandingPage.tsx
@@ -1,101 +1,106 @@
 import React from "react";
-import { graphql } from "gatsby";
+// import { graphql } from "gatsby";
 
-import {
-  // Hero,
-  // Events,
-  // About,
-  // Testimonials,
-  // Services,
-  // Products,
-  // Videos,
-  // ContactForm,
-  Layout,
-  Navbar,
-  Footer,
-} from "components";
+// import {
+//   // Hero,
+//   // Events,
+//   // About,
+//   // Testimonials,
+//   // Services,
+//   // Products,
+//   // Videos,
+//   // ContactForm,
+//   Layout,
+//   Navbar,
+//   Footer,
+// } from "components";
+const a = () => {
+  return null;
+}
 
-const LandingPage: React.FC<any> = ({
-  hero,
-  about,
-  testimonials,
-  services,
-  products,
-  videos,
-}) => {
-  return (
-    <>
-      {/* <Hero hero={hero} /> */}
-      {/* <Events /> */}
-      {/* <About about={about} />
-      <Testimonials testimonials={testimonials} />
-      <Services services={services} />
-      <Products products={products} />
-      <Videos videos={videos} />
-      <ContactForm /> */}
-    </>
-  );
-};
+export default a;
 
-const LandingPageLoader: React.FC<any> = (props) => {
-  const landing = props.data.main.nodes[0].frontmatter.pages.landing;
-  return (
-    <Layout>
-      <Navbar />
-      <main>
-        <LandingPage {...landing} />
-      </main>
-      <Footer />
-    </Layout>
-  );
-};
+// const LandingPage: React.FC<any> = ({
+//   hero,
+//   about,
+//   testimonials,
+//   services,
+//   products,
+//   videos,
+// }) => {
+//   return (
+//     <>
+//       {/* <Hero hero={hero} /> */}
+//       {/* <Events /> */}
+//       {/* <About about={about} />
+//       <Testimonials testimonials={testimonials} />
+//       <Services services={services} />
+//       <Products products={products} />
+//       <Videos videos={videos} />
+//       <ContactForm /> */}
+//     </>
+//   );
+// };
 
-export default LandingPageLoader;
+// const LandingPageLoader: React.FC<any> = (props) => {
+//   const landing = props.data.main.nodes[0].frontmatter.pages.landing;
+//   return (
+//     <Layout>
+//       <Navbar />
+//       <main>
+//         <LandingPage {...landing} />
+//       </main>
+//       <Footer />
+//     </Layout>
+//   );
+// };
 
-export const pageQuery = graphql`
-  query LandingPageQuery {
-    main: allMarkdownRemark(
-      filter: { frontmatter: { key: { eq: "landing" } } }
-    ) {
-      nodes {
-        frontmatter {
-          pages {
-            landing {
-              hero {
-                title
-                image
-              }
-              about {
-                text
-                image
-              }
-              testimonials {
-                author
-                role
-                testimonial
-              }
-              services {
-                title
-                name
-                description
-                image
-              }
-              products {
-                description
-                image
-                title
-                subtitle
-                link
-              }
-              videos {
-                title
-                subtitle
-                link
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// export default LandingPageLoader;
+
+// export const pageQuery = graphql`
+//   query LandingPageQuery {
+//     main: allMarkdownRemark(
+//       filter: { frontmatter: { key: { eq: "landing" } } }
+//     ) {
+//       nodes {
+//         frontmatter {
+//           pages {
+//             landing {
+//               hero {
+//                 title
+//                 image
+//               }
+//               about {
+//                 text
+//                 image
+//               }
+//               testimonials {
+//                 author
+//                 role
+//                 testimonial
+//               }
+//               services {
+//                 title
+//                 name
+//                 description
+//                 image
+//               }
+//               products {
+//                 description
+//                 image
+//                 title
+//                 subtitle
+//                 link
+//               }
+//               videos {
+//                 title
+//                 subtitle
+//                 link
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
