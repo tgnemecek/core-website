1ccddb0a2cf178bf47982fd8f551c7dd8d842d25
thiagonemecek <tgnemecek@gmail.com>
2020-11-22 02:35:09 -0500

    Test12

--- src/templates/LandingPage.tsx
+++ src/templates/LandingPage.tsx
@@ -1,65 +1,67 @@
-// import React from "react";
-// import { graphql } from "gatsby";
+import React from "react";
+import { graphql } from "gatsby";
 
-// import {
-//   // Hero,
-//   // Events,
-//   // About,
-//   // Testimonials,
-//   // Services,
-//   // Products,
-//   // Videos,
-//   // ContactForm,
-//   // Layout,
-//   Navbar,
-//   Footer,
-// } from "components/index";
+import {
+  // Hero,
+  // Events,
+  // About,
+  // Testimonials,
+  // Services,
+  // Products,
+  // Videos,
+  // ContactForm,
+  // Layout,
+  Navbar,
+  Footer,
+} from "components";
 
-// const LandingPage: React.FC<any> = ({
-//   hero,
-//   about,
-//   testimonials,
-//   services,
-//   products,
-//   videos,
-// }) => {
-//   return (
-//     <>
-//       {/* <Hero hero={hero} /> */}
-//       {/* <Events /> */}
-//       {/* <About about={about} />
-//       <Testimonials testimonials={testimonials} />
-//       <Services services={services} />
-//       <Products products={products} />
-//       <Videos videos={videos} />
-//       <ContactForm /> */}
-//     </>
-//   );
-// };
+const LandingPage: React.FC<any> = ({
+  hero,
+  about,
+  testimonials,
+  services,
+  products,
+  videos,
+}) => {
+  return (
+    <>
+      {/* <Hero hero={hero} /> */}
+      {/* <Events /> */}
+      {/* <About about={about} />
+      <Testimonials testimonials={testimonials} />
+      <Services services={services} />
+      <Products products={products} />
+      <Videos videos={videos} />
+      <ContactForm /> */}
+    </>
+  );
+};
 
-// const LandingPageLoader: React.FC<any> = (props) => {
-//   const landing = props.data.main.nodes[0].frontmatter.pages.landing;
-//   return (
-//     <>
-//       {/* <Navbar /> */}
-//       <main>
-//         <LandingPage {...landing} />
-//       </main>
-//       <Footer />
-//     </>
-//   );
-//   // return (
-//   //   <Layout>
-//   //     <Navbar />
-//   //     <main>
-//   //       <LandingPage {...landing} />
-//   //     </main>
-//   //     <Footer />
-//   //   </Layout>
-//   // );
-// };
+const LandingPageLoader: React.FC<any> = (props) => {
+  // const landing = props.data.main.nodes[0].frontmatter.pages.landing;
+  return (
+    <>
+      {/* <Navbar /> */}
+      <main>
+        <LandingPage
+        //  {...landing} 
+         />
+      </main>
+      <Footer />
+    </>
+  );
+  // return (
+  //   <Layout>
+  //     <Navbar />
+  //     <main>
+  //       <LandingPage {...landing} />
+  //     </main>
+  //     <Footer />
+  //   </Layout>
+  // );
+};
 
-// export default LandingPageLoader;
+export default LandingPageLoader;
 
 // export const pageQuery = graphql`
 //   query LandingPageQuery {
@@ -108,89 +110,3 @@
 //     }
 //   }
 // `;
-import React from "react";
-import { graphql } from "gatsby";
-import leadingReport from "src/downloads/free-reports/leading.pdf";
-import { getContactEmail } from "utils";
-
-import {
-  Hero,
-  Section,
-  Explanation,
-  Benefits,
-  CallToAction,
-  FreeReport,
-  PayPalButtons,
-  ContactForm,
-  Layout,
-  Navbar,
-  Footer,
-} from "components";
-
-const LandingPage: React.FC<any> = ({ hero, benefits, explanation, email }) => {
-  return (
-    <>
-      <Hero hero={hero} small={true} />
-      <Section>
-        <Explanation explanation={explanation} />
-        <FreeReport
-          reportText="Get a Free Leader Compass Report!"
-          downloadLink={leadingReport}
-        />
-        <Benefits
-          benefits={benefits}
-          title="Schedule a time with us if you are:"
-        />
-        <CallToAction href="#contact-form" text="Send Us a message" />
-      </Section>
-      <Section>
-        <PayPalButtons
-          buttonTypes={["leaderStrengths", "entrepreneuerStrengths"]}
-        />
-      </Section>
-      <ContactForm />
-    </>
-  );
-};
-
-const LandingPageLoader: React.FC<any> = (props) => {
-  const leading = props.data.main.nodes[0].frontmatter.pages.leading;
-
-  return (
-    <Layout>
-      <Navbar />
-      <main>
-        <LandingPage {...leading} email={getContactEmail()} />
-      </main>
-      <Footer />
-    </Layout>
-  );
-};
-
-export default LandingPageLoader;
-
-export const pageQuery = graphql`
-  query LandingPageQuery {
-    main: allMarkdownRemark(
-      filter: { frontmatter: { key: { eq: "leading" } } }
-    ) {
-      nodes {
-        frontmatter {
-          pages {
-            leading {
-              benefits
-              explanation {
-                text
-                image
-              }
-              hero {
-                title
-                image
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
