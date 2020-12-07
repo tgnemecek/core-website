b8fdec51d2177ea2fead81218c2f9e5c4b8e8a77
thiagonemecek <tgnemecek@gmail.com>
2020-12-06 21:57:10 -0500

    Add Section to ContentGrid

--- src/templates/EventPage/sections/ContentGrid/index.tsx
+++ src/templates/EventPage/sections/ContentGrid/index.tsx
@@ -1,29 +1,22 @@
 import React from "react";
-import moment from "moment";
-import {
-  Container,
-  Grid,
-  Card,
-  CardContent,
-  CardActions,
-  Typography,
-} from "@material-ui/core";
+import { Container } from "@material-ui/core";
+import { Section } from "components";
 import { makeStyles } from "@material-ui/core/styles";
 
 const ContentGrid: React.FC = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <Container>
-      <section className={classes.section}>{children}</section>
-    </Container>
+    <Section>
+      <Container className={classes.container}>{children}</Container>
+    </Section>
   );
 };
 
 export default ContentGrid;
 
 const useStyles = makeStyles((theme) => ({
-  section: {
+  container: {
     display: "grid",
     gridTemplateColumns: "2fr 1fr",
   },
