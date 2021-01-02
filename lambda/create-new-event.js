87986ab1cd0c361c02a0b94bbf44d319bf7e3342
thiagonemecek <tgnemecek@gmail.com>
2021-01-01 19:19:20 -0500

    Finished zoom api, starting google calendar

--- lambda/create-new-event.js
+++ lambda/create-new-event.js
@@ -1,3 +1,11 @@
+const jwt = require("jsonwebtoken");
+const fetch = require("node-fetch");
+const readline = require("readline");
+const { google } = require("googleapis");
+
+// Zoom Documentation for this endpoint can be found here:
+// https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingcreate
+
 function makeid(length) {
   var result = "";
   var characters =
@@ -10,22 +18,77 @@ function makeid(length) {
 }
 
 module.exports.handler = async (event, context) => {
-  // const fetch = require("node-fetch");
   console.log("HERE!");
   try {
-    const body = JSON.parse(event.body);
+    const body = JSON.parse(event.body || "{}");
 
-    const meetingID = makeid(8);
-    const calendarID = makeid(8);
+    const token = jwt.sign(
+      {
+        iss: process.env.ZOOM_API_KEY,
+        exp: 1496091964000,
+      },
+      process.env.ZOOM_API_SECRET
+    );
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        ...body,
-        meetingID,
-        calendarID,
-      }),
-    };
+    const res = await fetch(
+      `https://api.zoom.us/v2/users/${process.env.ZOOM_USER_ID}/meetings`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "User-Agent": "Zoom-api-Jwt-Request",
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          topic: "Topic here",
+          type: 2,
+          start_time: "2021-09-28T23:00:00.000Z",
+          duration: 15,
+          timezone: "America/New_York",
+          password: "1234",
+          agenda: "agenda here",
+        }),
+      }
+    );
+
+    if (res.status === 201) {
+      const { id: meetingID } = await res.json();
+
+      // function initClient() {
+      //   google.client.init({
+      //     apiKey: API_KEY,
+      //     clientId: CLIENT_ID,
+      //     discoveryDocs: DISCOVERY_DOCS,
+      //     scope: SCOPES
+      //   }).then(function () {
+      //     // Listen for sign-in state changes.
+      //     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      //     // Handle the initial sign-in state.
+      //     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      //     authorizeButton.onclick = handleAuthClick;
+      //     signoutButton.onclick = handleSignoutClick;
+      //   }, function(error) {
+      //     appendPre(JSON.stringify(error, null, 2));
+      //   });
+      // }
+
+      console.log({ meetingID });
+
+      const calendarID = makeid(8);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          ...body,
+          meetingID,
+          calendarID,
+        }),
+      };
+    }
+
+    console.log({ res });
+
     // // Auth
     // const token = `token=${process.env.EVENTBRITE_API_TOKEN}`;
     // const orgId = process.env.EVENTBRITE_ORGANIZATION_ID;
