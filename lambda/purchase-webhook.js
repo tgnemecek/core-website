6e5a274866f196367bcda081b7b37cdcbfb675da
thiagonemecek <tgnemecek@gmail.com>
2021-02-03 18:39:22 -0500

    Add signature to webhook

--- lambda/purchase-webhook.js
+++ lambda/purchase-webhook.js
@@ -6,8 +6,19 @@ const moment = require("moment");
 module.exports.handler = async (event, context) => {
   try {
     const body = JSON.parse(event.body || "{}");
+    const signature = event.headers["stripe-signature"];
 
-    console.log({ body });
+    console.log(body.data);
+
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_PURCHASE_ENDPOINT_SECRET
+    );
+
+    if (event.type !== "payment_intent.succeeded") {
+      throw new Error(`Payment Intent type is incorrect, got: ${event.type}`);
+    }
 
     return {
       statusCode: 200,
