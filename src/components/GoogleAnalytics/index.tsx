0704492ebf18a86cdc4cf965b96f0d986c70661e
thiagonemecek <tgnemecek@gmail.com>
2021-08-05 19:45:37 -0400

    Add console.log

--- src/components/GoogleAnalytics/index.tsx
+++ src/components/GoogleAnalytics/index.tsx
@@ -8,6 +8,7 @@ const GoogleAnalytics: React.FC = () => {
   const { pathname } = useLocation();
 
   React.useEffect(() => {
+    console.log({ GATSBY_GA_TRACKING_ID });
     if (typeof window !== "undefined" && GATSBY_GA_TRACKING_ID) {
       ReactGA.initialize(GATSBY_GA_TRACKING_ID);
       ReactGA.pageview(pathname);
