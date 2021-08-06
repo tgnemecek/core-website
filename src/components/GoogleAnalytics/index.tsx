f2831c0c7a25c752f5b47c803c78b29fe15c288f
thiagonemecek <tgnemecek@gmail.com>
2021-08-05 20:54:18 -0400

    Remove console.log

--- src/components/GoogleAnalytics/index.tsx
+++ src/components/GoogleAnalytics/index.tsx
@@ -2,21 +2,14 @@ import React from "react";
 import ReactGA from "react-ga";
 import { useLocation } from "@reach/router";
 
-// const { GATSBY_GA_TRACKING_ID } = process.env;
-const GATSBY_GA_TRACKING_ID = "UA-178536442-1";
+const { GATSBY_GA_TRACKING_ID } = process.env;
 
 const GoogleAnalytics: React.FC = () => {
   const { pathname } = useLocation();
 
   React.useEffect(() => {
-    console.log({ GATSBY_GA_TRACKING_ID });
     if (typeof window !== "undefined" && GATSBY_GA_TRACKING_ID) {
-      ReactGA.initialize(GATSBY_GA_TRACKING_ID, {
-        debug: true,
-        // gaOptions: {
-        //   siteSpeedSampleRate: 100,
-        // },
-      });
+      ReactGA.initialize(GATSBY_GA_TRACKING_ID);
       ReactGA.pageview(pathname);
     }
   }, []);
