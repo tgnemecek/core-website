0f105f2e4135145754e0eaf797a301aa6f10fcd6
thiagonemecek <tgnemecek@gmail.com>
2021-08-06 18:06:26 -0400

    Add typeof window

--- src/utils/getVideoImage.ts
+++ src/utils/getVideoImage.ts
@@ -1,9 +1,13 @@
 const getVideoImage = (videoId: string) => {
-  const {
-    location: { protocol },
-  } = window;
+  if (typeof window !== "undefined") {
+    const {
+      location: { protocol },
+    } = window;
 
-  return `${protocol}//img.youtube.com/vi/${videoId}/mqdefault.jpg`;
+    return `${protocol}//img.youtube.com/vi/${videoId}/mqdefault.jpg`;
+  }
+
+  return "";
 };
 
 export default getVideoImage;
