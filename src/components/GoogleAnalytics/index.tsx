13c5e6fbda7838d0320ca81e4c24c70d107daac9
thiagonemecek <tgnemecek@gmail.com>
2021-08-05 20:46:27 -0400

    Fix conflicts

--- src/components/GoogleAnalytics/index.tsx
+++ src/components/GoogleAnalytics/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactGA from "react-ga";
 import { useLocation } from "@reach/router";
 
-const { GATSBY_GA_TRACKING_ID } = process.env;
+// const { GATSBY_GA_TRACKING_ID } = process.env;
+const GATSBY_GA_TRACKING_ID = "UA-178536442-1";
 
 const GoogleAnalytics: React.FC = () => {
   const { pathname } = useLocation();
@@ -10,7 +11,12 @@ const GoogleAnalytics: React.FC = () => {
   React.useEffect(() => {
     console.log({ GATSBY_GA_TRACKING_ID });
     if (typeof window !== "undefined" && GATSBY_GA_TRACKING_ID) {
-      ReactGA.initialize(GATSBY_GA_TRACKING_ID);
+      ReactGA.initialize(GATSBY_GA_TRACKING_ID, {
+        debug: true,
+        // gaOptions: {
+        //   siteSpeedSampleRate: 100,
+        // },
+      });
       ReactGA.pageview(pathname);
     }
   }, []);
